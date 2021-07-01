77a8e360f5281203ccf49b6edcf69be265efe819
redpandaathome <leeyumi415@gmail.com>
2021-07-01 10:10:06 +0900

    GetLineItem test

--- publisher-management-api/client/client.go
+++ publisher-management-api/client/client.go
@@ -5,3 +5,15 @@ import "fmt"
 func SayHi(name string) string {
 	return fmt.Sprintf("hello~~~~~~~!!!!! %s", name)
 }
+
+type Client interface {
+	GetLineItem(aa string) (string, error)
+}
+
+type ApiClient struct {
+	ApiKey string `json:"api_key"`
+}
+
+func (a ApiClient) GetLineItem(aa string) (string, error) {
+	return aa, nil
+}
